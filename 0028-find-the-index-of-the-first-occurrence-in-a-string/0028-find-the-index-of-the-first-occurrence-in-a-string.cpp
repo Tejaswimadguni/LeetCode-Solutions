4ec4846580a59ddae407dbf7cf044555e348081f
TEJASWI NAGARAJ MADGUNI <tmadguni@gmail.com>
2026-04-01 23:12:13 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -3,20 +3,14 @@ public:
     int strStr(string haystack, string needle) {
         int m=needle.length();
         int n=haystack.length();
-        int s=0;
-        for(int i=0;i<n;i++){
-         s=0;
-         int r=0;
-         for(int j=i;j<i+m;j++){
-            if(haystack[j]==needle[r]){
-               s++;
-               r++;
-            }else{break;}
-            
-         }
-         if(s==m){
-            return i;
-         }
+        for(int i=0;i<n-m;i++){
+            int j=0;
+            while(j<m && haystack[i+j]==needle[j]){
+                j++;
+            }
+            if(m==j){
+                return i;
+            }
         }
         return -1;
     }
