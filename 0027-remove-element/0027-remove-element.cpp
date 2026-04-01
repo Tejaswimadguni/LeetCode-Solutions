a38d547e3450ac8160e03442b728243ad1b03fd5
TEJASWI NAGARAJ MADGUNI <tmadguni@gmail.com>
2026-04-01 20:16:29 +0530

    Time: 0 ms (100%), Space: 11.7 MB (83.27%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -1,25 +1,13 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-          vector<int> answer;
-        int m=0;
-        int s=nums.size();
-        for(int i=0;i<nums.size();i++){
-          if(nums[i]==val){
-               m++;
+          int i=0;
+      for(int j=0;j<nums.size();j++){
+          if(nums[j]!=val){
+               nums[i]=nums[j];
+               i++;
           }
-          
-          }
-          int ans=s-m;
-          answer=nums;
-          nums.clear();
-
-     for(int i=0;i<answer.size();i++){
-          if(answer[i]==val){
-               continue;
-          }
-          nums.push_back(answer[i]);
-     }
-     return ans;
+      }
+     return i;
     }
 };
