a311173170df86f00268844b2cf6fd2db15e7a00
TEJASWI NAGARAJ MADGUNI <tmadguni@gmail.com>
2026-04-03 20:04:18 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,26 +1,11 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int i=0,j=0,k=0;
-    int n=nums.size();
-
-    while(i<n){
-        if(i!=j && (nums[i] ^ nums[j])==0){
-            k++;
+        int n=nums.size();
+        int ans=0;
+        for(int i:nums){
+            ans ^=i;
         }
-
-        j++;
-
-        if(j==n){
-            if(k==0){
-                return nums[i];
-            }
-            i++;
-            j=0;
-            k=0;
-        }
-    }
-
-    return -1;
+        return ans;
     }
 };
