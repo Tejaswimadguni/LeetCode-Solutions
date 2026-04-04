2eb13f941448833e3ca189cdcd8967ed80f669b7
TEJASWI NAGARAJ MADGUNI <tmadguni@gmail.com>
2026-04-04 12:37:16 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,46 +1,27 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> ans;
+         unordered_map<int,int> mp;
+        int m = nums2.size();
+        for(int i = 0; i < m; i++){
 
-    int n = nums1.size();
-    int m = nums2.size();
+            int next = -1;
 
-    int i = 0, j = 0;
-
-    while(i < n) {
-
-        int s = nums1[i];
-
-        while(j < m) {
-
-            if(s == nums2[j]) {
-
-                bool found = false;
-
-                for(int x = j + 1; x < m; x++) {
-
-                    if(nums2[x] > nums2[j]) {
-                        ans.push_back(nums2[x]);
-                        found = true;
-                        break;
-                    }
+            for(int j = i + 1; j < m; j++){
+                if(nums2[j] > nums2[i]){
+                    next = nums2[j];
+                    break;
                 }
-
-                if(!found) {
-                    ans.push_back(-1);
-                }
-
-                i++;
-                j = 0;
-                break;
             }
 
-            j++;
+            mp[nums2[i]] = next;
         }
-    }
+        vector<int> ans;
 
-    return ans;
-        
+        for(int x : nums1){
+            ans.push_back(mp[x]);
+        }
+
+        return ans;
     }
 };
