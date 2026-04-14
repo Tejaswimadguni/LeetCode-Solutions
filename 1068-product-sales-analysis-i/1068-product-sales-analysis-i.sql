2c197be5525a6dcf524a32e63407e3b4ffd6e8ec
TEJASWI NAGARAJ MADGUNI <tmadguni@gmail.com>
2026-04-14 13:12:26 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
+++ 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
@@ -1,5 +1,3 @@
 # Write your MySQL query statement below
-select p.product_name as product_name,S.year as year,S.price as price 
-from Sales S
-Join product P
-on S.product_id=P.product_id;
+SELECT product_name, year, price FROM sales
+JOIN product ON sales.product_id = product.product_id;
