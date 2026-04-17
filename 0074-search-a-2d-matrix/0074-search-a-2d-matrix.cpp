b8882db6c97dc31acbb794437f474aae83c49466
TEJASWI NAGARAJ MADGUNI <tmadguni@gmail.com>
2026-04-17 22:37:46 +0530

    Time: 0 ms (100%), Space: 13.4 MB (44.27%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+          int row=matrix .size();
+        int col=matrix[0] .size();
+        int s=0;
+        int e=row*col-1;
+        while(s<=e){
+            int mid=s+(e-s)/2;
+            if(matrix[mid/col][mid%col]==target){
+                return true;
+            }
+            else if(matrix[mid/col][mid%col]>target){
+                e=mid-1;
+            }else{s=mid+1;}
+        }
+        return 0;
+    }
+};
