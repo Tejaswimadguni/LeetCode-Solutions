e8039735923f4e1494f0c20cbd56d4828ea9fa21
TEJASWI NAGARAJ MADGUNI <tmadguni@gmail.com>
2026-04-18 23:58:11 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -4,15 +4,13 @@ public:
         int cnt=0;
         vector<bool>prime(n+1,true);
         prime[0]=prime[1]=false;
-        for(int i=2;i*i<n;i++){
+        for(int i=2;i<n;i++){
             if(prime[i]){
-                for(int j=i*i;j<n;j=j+i){
+                cnt++;
+                for(int j=2*i;j<n;j=j+i){
                     prime[j]=false;
                 }
             }
-        }
-         for(int i = 2; i < n; i++){
-            if(prime[i]) cnt++;
         }
         return cnt;
     }
