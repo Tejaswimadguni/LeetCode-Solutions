0ead53887c8dd2d32291099c8b9961683ec0a57f
TEJASWI NAGARAJ MADGUNI <tmadguni@gmail.com>
2026-04-24 12:13:00 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2833-furthest-point-from-origin/2833-furthest-point-from-origin.cpp
+++ 2833-furthest-point-from-origin/2833-furthest-point-from-origin.cpp
@@ -1,25 +1,13 @@
 class Solution {
 public:
     int furthestDistanceFromOrigin(string moves) {
-           int n=moves.length();
-        int l=0,r=0;
-        for(int i=0;i<n;i++){
-          if(moves[i]=='_')continue;
-          if(moves[i]=='L')l++;
-          else if(moves[i]=='R')r++;
-        }
-        int count=0;
-        count=max(l,r);
-        int ans=0;
-        for(char ch:moves){
-          if(ch=='L')ans--;
-          else if(ch=='R')ans++;
-          else if(ch=='_'){
-            if(count==l){
-              ans--;
-            }else{ans++;}
-          }
-        }
-        return abs(ans);
+           int l=0,r=0,u=0;
+       for(char ch:moves){
+        if(ch=='L')l++;
+        else if(ch=='R')r++;
+        else{u++;}
+       }
+
+       return abs(l-r)+u;
     }
 };
