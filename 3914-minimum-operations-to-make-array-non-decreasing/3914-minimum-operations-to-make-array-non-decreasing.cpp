49082d0805a0933f00697bfdb559c756efbae9e1
TEJASWI NAGARAJ MADGUNI <tmadguni@gmail.com>
2026-04-27 11:28:33 +0530

    Time: 0 ms (100%), Space: 137 MB (0%) - LeetHub

---
+++ 3914-minimum-operations-to-make-array-non-decreasing/3914-minimum-operations-to-make-array-non-decreasing.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    long long minOperations(vector<int>& nums) {
+        int n=nums .size();
+
+        long long ans=0;
+        for(int i=0;i<n-1;i++){
+          ans+=max(0,nums[i]-nums[i+1]);
+        }
+        return ans;
+    }
+};
