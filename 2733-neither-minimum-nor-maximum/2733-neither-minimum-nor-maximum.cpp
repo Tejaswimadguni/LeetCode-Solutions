677f0dce999af3b1813acf8fafafcfceca92eb91
TEJASWI NAGARAJ MADGUNI <tmadguni@gmail.com>
2026-05-01 09:47:04 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2733-neither-minimum-nor-maximum/2733-neither-minimum-nor-maximum.cpp
+++ 2733-neither-minimum-nor-maximum/2733-neither-minimum-nor-maximum.cpp
@@ -1,20 +1,8 @@
 class Solution {
 public:
     int findNonMinOrMax(vector<int>& nums) {
-         int n=nums .size();
-    vector<int> ans;
-    int i=1;
-    sort(nums.begin(),nums.end());
-    ans.push_back(nums[0]);
-    while( i<n){
-       if(nums[i]!=nums[i-1]){
-        ans.push_back(nums[i]);
-        i++;
-       }else{i++;}
-
-    } 
-    if(ans.size()<3)return -1;
-    return ans[1];
-
+        sort(nums.begin(),nums.end());
+        if(nums.size()<3)return -1;
+        return nums[1];
     }
 };
