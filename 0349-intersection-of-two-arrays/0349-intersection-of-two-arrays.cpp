fdff0d4d310f77fdaacca063b0785512314e30de
TEJASWI NAGARAJ MADGUNI <tmadguni@gmail.com>
2026-05-16 17:18:50 +0530

    Time: 0 ms (100%), Space: 14.4 MB (39.18%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -1,36 +1,16 @@
 class Solution {
-    vector<int> removeDuplicates(vector<int>& nums) {
-
-    unordered_set<int> seen;
-    vector<int> ans;
-
-    for(int x : nums) {
-
-        if(seen.find(x) == seen.end()) {
-            seen.insert(x);
-            ans.push_back(x);
-        }
-    }
-  return ans;
-   
-}
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-    vector<int> ans;
+        unordered_set<int> s(nums1.begin(), nums1.end());
+    unordered_set<int> ans;
 
-    for(int i = 0; i < nums1.size(); i++) {
+    for(int x : nums2) {
 
-        for(int j = 0; j < nums2.size(); j++) {
-
-            if(nums1[i] == nums2[j]) {
-                ans.push_back(nums1[i]);
-            }
+        if(s.find(x) != s.end()) {
+            ans.insert(x);
         }
     }
 
-    ans = removeDuplicates(ans);
-
- 
-            return ans;
+    return vector<int>(ans.begin(), ans.end());
     }
 };
