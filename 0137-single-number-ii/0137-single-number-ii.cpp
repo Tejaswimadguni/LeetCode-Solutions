6e46f9b505102cfc9b4f54e949fa58d771e0f5da
TEJASWI NAGARAJ MADGUNI <tmadguni@gmail.com>
2026-05-18 18:52:26 +0530

    Time: 4 ms (13.84%), Space: 14.1 MB (10.49%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_map<int,int>ans;
+        for(int i:nums){
+            ans[i]++;
+        }
+        for(auto &i:ans){
+            if(i.second==1)return i.first;
+        }
+        return 0;
+    }
+};
