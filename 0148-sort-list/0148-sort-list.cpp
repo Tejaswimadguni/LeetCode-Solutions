d0bc4471e2772a0a17e9b4fad33ecebac9023b5b
TEJASWI NAGARAJ MADGUNI <tmadguni@gmail.com>
2026-05-28 12:11:28 +0530

    Time: 7 ms (95.4%), Space: 58.1 MB (57.98%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        ListNode* temp=head;
+        vector<int>ans;
+        while(temp){
+            ans.push_back(temp->val);
+            temp=temp->next;
+        }
+        sort(ans.begin(),ans.end());
+        temp=head;
+        int i=0;
+        int n=ans.size();
+        while(temp && i<n){
+            temp->val=ans[i];
+            temp=temp->next;
+            i++;
+        }
+        return head;
+    }
+};
