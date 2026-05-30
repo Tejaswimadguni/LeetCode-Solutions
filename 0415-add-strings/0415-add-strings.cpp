aed1e290cdf7a0066400eaba012535caf3ab8071
TEJASWI NAGARAJ MADGUNI <tmadguni@gmail.com>
2026-05-30 12:12:40 +0530

    Time: 0 ms (100%), Space: 9.2 MB (58.02%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string addStrings(string num1, string num2) {
+        int i=num1.size()-1;
+        int j=num2.size()-1;
+        int carry =0;
+        string ans="";
+        while(i>=0 || j>=0 || carry){
+          int d1 = (i >= 0) ? num1[i] - '0' : 0;
+          int d2 = (j >= 0) ? num2[j] - '0' : 0;  
+            int sum=d1+d2+carry;
+            ans.push_back((sum%10)+'0');
+            carry=sum/10;
+            i--;
+            j--;
+        }
+
+        reverse(ans.begin(),ans.end());
+
+        return ans;
+    }
+};
