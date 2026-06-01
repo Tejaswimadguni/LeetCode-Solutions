27c197ea884c909fb14de50513c83fbc33de9b23
TEJASWI NAGARAJ MADGUNI <tmadguni@gmail.com>
2026-06-01 18:00:49 +0530

    Time: 3 ms (58.11%), Space: 8.9 MB (41.3%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -1,10 +1,16 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-         if (n == 1)
+        if(n<1)return false;
+        if(n==1)return true;
+        while(n>1){
+            if(n%3!=0){
+                return false;
+            }else{
+                n/=3;
+            }
+        }
+
         return true;
-        if (n <= 0 || n % 3 != 0)
-        return false;
-        return isPowerOfThree(n / 3);
     }
 };
