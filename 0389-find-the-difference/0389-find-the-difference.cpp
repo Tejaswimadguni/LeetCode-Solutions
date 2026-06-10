af49112e397917ba696690187495365e4a69bae8
TEJASWI NAGARAJ MADGUNI <tmadguni@gmail.com>
2026-06-10 13:18:58 +0530

    Time: 0 ms (100%), Space: 9.2 MB (49.64%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+         vector<int> freq(26, 0);
+        for(char ch : s)
+            freq[ch - 'a']++;
+        for(char ch : t)
+            freq[ch - 'a']--;
+        for(int i = 0; i < 26; i++) {
+            if(freq[i] < 0)
+                return i + 'a';
+        }
+        return ' ';
+    }
+};
