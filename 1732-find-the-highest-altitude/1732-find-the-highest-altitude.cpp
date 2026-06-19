aa06b3b85ab7f3063bbf141ea02ef45a338c0adb
TEJASWI NAGARAJ MADGUNI <tmadguni@gmail.com>
2026-06-19 11:45:42 +0530

    Time: 0 ms (100%), Space: 10.6 MB (94.12%) - LeetHub

---
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int ans=0;
+        int sum=0;
+        for(int i:gain){
+            sum+=i;
+            ans=max(ans,sum);
+        }
+        return ans;
+    }
+};
