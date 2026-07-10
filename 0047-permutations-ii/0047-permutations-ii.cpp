8dc0afd132d5bf62ff0c5a0e60d2f79b905b4841
TEJASWI NAGARAJ MADGUNI <tmadguni@gmail.com>
2026-07-10 11:58:14 +0530

    Time: 0 ms (100%), Space: 11.1 MB (99.67%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -1,35 +1,12 @@
-
 class Solution {
-
-    private:
-    bool exists(vector<vector<int>>& ans, vector<int>&nums) {
-    for (auto &row : ans) {
-        if (row == nums)
-            return true;
-    }
-    return false;
-}
-    void solve(vector<int>nums,int indx,vector<vector<int>>&ans){
-        if(indx>=nums.size()){
-            if(!exists(ans,nums)){
-            ans.push_back(nums);
-            }
-            return;
-            
-        }
-
-        for(int i=indx;i<nums.size();i++){
-            swap(nums[i],nums[indx]);
-            solve(nums,indx+1,ans);
-            swap(nums[i],nums[indx]);
-        }
-
-    }
 public:
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        vector<vector<int>>ans;
-        int indx=0;
-        solve(nums,indx,ans);
+        vector<vector<int>> ans;
+        sort(nums.begin(), nums.end());
+        ans.push_back(nums);
+        while(next_permutation(nums.begin(), nums.end())){
+            ans.push_back(nums);
+        }
         return ans;
     }
 };
