9e9aad7b8c3fa9c38ff829b3361a84d8726a5e3a
TEJASWI NAGARAJ MADGUNI <tmadguni@gmail.com>
2026-07-16 20:06:51 +0530

    Time: 0 ms (100%), Space: 8.6 MB (9.28%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -1,15 +1,6 @@
 class Solution {
 public:
     int gcdOfOddEvenSums(int n) {
-        int od=0,ev=0;
-        int cnt=0;
-        for(int i=1;i<=2*n;i++){
-            if(i%2==0){
-                ev+=i;
-            }else{
-                od+=i;
-            }
-        }
-        return gcd(od,ev);
+        return n;
     }
 };
