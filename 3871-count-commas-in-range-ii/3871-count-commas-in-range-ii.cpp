415d312ab5d79ba6d109caa34accaa92cf215e8a
TEJASWI NAGARAJ MADGUNI <tmadguni@gmail.com>
2026-09-09 20:01:06 +0530

    Time: 0 ms (100%), Space: 9.4 MB (7.73%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
@@ -1,13 +1,36 @@
 class Solution {
 public:
     long long countCommas(long long n) {
-        
-        long long ans = 0;
-
-        for (long long x = 1000; x <= n; x *= 1000) {
-            ans += n - x + 1;
+        if(n<1000)return 0;
+        map<int,long long>mp;
+        mp[1] = 999999 - 1000 + 1;
+        mp[2] = 999999999 - 1000000 + 1;
+        mp[3] = 999999999999LL - 1000000000LL + 1;
+        mp[4] = 999999999999999LL - 1000000000000LL + 1;
+        mp[5] = 999999999999999999LL - 1000000000000000LL + 1;
+        mp[6] = 9223372036854775807LL - 1000000000000000000LL + 1;
+        long long ans=0;
+        if(n<=999999){
+            ans+=n-999;
+        }else if(n<=999999999){
+            ans+=(n-1000000 + 1)*2;
+            ans+=mp[1];
+        }else if(n<=999999999999LL){
+            ans+=(n-1000000000LL + 1)*3;
+            ans+=mp[1]+mp[2]*2;
+        }else if(n<=999999999999999LL){
+            ans+=(n-1000000000000LL + 1)*4;
+            ans+=mp[1]+mp[2]*2+mp[3]*3;
+        }else if(n<=999999999999999999LL){
+            ans+=(n-1000000000000000LL + 1)*5;
+            ans+=mp[1]+mp[2]*2+mp[3]*3+mp[4]*4;
+        }else if(n<=9223372036854775807LL){
+            ans+=(n-1000000000000000000LL + 1)*6;
+            ans+=mp[1]+mp[2]*2+mp[3]*3+mp[4]*4+mp[5]*5;
         }
 
         return ans;
+
+
     }
 };
